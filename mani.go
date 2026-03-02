bd349ad5275e4581b36327cafb8e6d9467b0dfea
@eduardomarini <eduardo.marini@ativasolucoes.com.br>
2026-03-02 12:01:28 -0300

    nível1: cria cabeçalho e salva arquivo excel

---
+++ mani.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/xuri/excelize/v2"
+)
+
+func main() {
+
+	// cria um novo arquivo
+	f := excelize.NewFile()
+
+	sheet := "Vendas"
+	f.SetSheetName("Sheet1", sheet)
+
+	// cabeçalho da planilha
+	headers := []string{
+		"Nome do Provedor",
+		"Cidade",
+		"Estado",
+		"Telefone",
+		"Nome do COntato",
+		"Data do Primeiro COntato",
+		"Status",
+	}
+
+	// preencher os cabeçalhos
+	for i, header := range headers {
+		cell, _ := excelize.CoordinatesToCellName(i+1, 1)
+		f.SetCellValue(sheet, cell, header)
+	}
+
+	// Estilo cabeçalho
+	style, err := f.NewStyle(&excelize.Style{
+		Font: &excelize.Font{
+			Bold: true,
+		},
+		Alignment: &excelize.Alignment{
+			Horizontal: "center",
+		},
+		Fill: excelize.Fill{
+			Type:    "pattern",
+			Color:   []string{"#D9D9D9"},
+			Pattern: 1,
+		},
+	})
+	if err != nil {
+		fmt.Println("Erro ao criar estilo:", err)
+		return
+	}
+
+	f.SetCellStyle(sheet, "A1", "G1", style)
+
+	// ajusta largura das colunas
+	f.SetColWidth(sheet, "A", "A", 25)
+	f.SetColWidth(sheet, "B", "C", 15)
+	f.SetColWidth(sheet, "D", "D", 18)
+	f.SetColWidth(sheet, "E", "E", 22)
+	f.SetColWidth(sheet, "F", "G", 20)
+
+	// Salva o arquivo
+	err = f.SaveAs("controle_vendas_provedores.xlsx")
+	if err != nil {
+		fmt.Println("Erro ao salvar arquivo:", err)
+		return
+	}
+
+	fmt.Println("Planilha criada com sucesso!!!")
+}
